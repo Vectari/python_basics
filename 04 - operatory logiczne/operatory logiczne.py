7d33f7cf87d9f2cb6ff12e94b22ae4fd48cd0487
Vectari <mateusz.majer996@gmail.com>
2024-08-07 16:31:11 +0200

    feat: example of not

--- 04 - operatory logiczne/operatory logiczne.py
+++ 04 - operatory logiczne/operatory logiczne.py
@@ -1,6 +1,10 @@
-a = True
-b = False
+a = 5
+b = 2
 
-print(a or b)
-print(a and b)
-print(a>10 or (b>0 and a<b))
+# print(a or b)
+# print(a and b)
+# print(a>10 or (b>0 and a<b))
+
+c = 5
+print(c % 2 == 0)
+print(not c % 2 == 0)
