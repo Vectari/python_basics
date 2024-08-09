3e2b6a5e145d7b4d928024705564c9fcbc3d49ef
Vectari <mateusz.majer996@gmail.com>
2024-08-09 22:19:37 +0200

    feat: add string methods

---
+++ 07 - metody strings/07-metodystrings.py
@@ -0,0 +1,8 @@
+channel_name = " super channel!!!      "
+
+print(channel_name.upper())
+print(channel_name.capitalize())
+print(channel_name.strip())
+print(channel_name.startswith(" "))
+print(channel_name.replace("super", "mega"))
+print(channel_name.split(" "))
