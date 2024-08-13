5e8386a173d9b1a98d17ce2975899dc1c42259ff
Vectari <mateusz.majer996@gmail.com>
2024-08-13 16:26:28 +0200

    feat: update set

--- lessons/16 - set{}/16 - set{}.py
+++ lessons/16 - set{}/16 - set{}.py
@@ -1,4 +1,40 @@
-names = {"Kamil", "Mariusz", "Dominik", "Kamil"}
-names.add("Radek")
+names = {"Mateusz", "Mariusz", "Dominik", "Kamil"}
+names2 = {"Mateusz", "Ola", "Klaudia", "Czesław"}
+names3 = set()
 
-print(names)
+print(type(names3))
+
+# names.add("Radek")
+# names.remove("Kamil")
+
+
+# print(names)
+
+
+# for name in names:
+#   print(name)
+
+
+# names.update({"Ola", "Ala"})
+# # LUB
+# names.update(["Ola2", "Ala2"])
+
+
+# for name in names:
+#   print(name)
+
+
+#SUMA ZBIORÓW
+print(names | names2)
+# LUB
+print(names.union(names2))
+
+#część wspólna
+print(names.intersection(names2))
+#LUB
+print(names & names2)
+
+#różnica
+print(names.difference(names2))
+#lub
+print(names - names2)
