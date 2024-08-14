2e41771e7a9cfc7d97d50688adc09e669ec2b482
Vectari <mateusz.majer996@gmail.com>
2024-08-14 21:03:29 +0200

    feat: add tuple

---
+++ lessons/18 - tuple()/18 - tuple().py
@@ -0,0 +1,14 @@
+names = ("Mateusz", "Mariusz", "Dominik")
+print(names)
+
+for name in names:
+  print(name)
+
+user = ("Mateusz", 28)
+user1 = ("Tomek", 20)
+user2 = ("Marek", 31)
+
+users = [user, user1, user2]
+
+for user in users:
+  print(user)
